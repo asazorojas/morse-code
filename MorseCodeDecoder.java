bfc3a3a196ef45b190d34054c0f953b8e4138ce1
mgruben <GrubenM@gmail.com>
2016-09-08 10:43:12 -0500

    Rename decodeBits as bitsToMorse
    Because that's what it does.

--- MorseCodeDecoder.java
+++ MorseCodeDecoder.java
@@ -84,7 +84,7 @@ public class MorseCodeDecoder {
      * @param bits
      * @return 
      */
-    public static String decodeBits(String bits) {
+    public static String bitsToMorse(String bits) {
         String morse = "";
         bits = bits.replaceAll("^[0]+", "");
         bits = bits.replaceAll("[0]+$", "");
@@ -139,7 +139,7 @@ public class MorseCodeDecoder {
      */
     public static void main(String[] args) {
         String bits = "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011";
-        String morse = MorseCodeDecoder.decodeBits(bits);
+        String morse = MorseCodeDecoder.bitsToMorse(bits);
         System.out.println(morse);
 //        System.out.println(
 //                MorseCodeDecoder.decode(msg));
