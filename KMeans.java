4aa6286bcbfb6312b1b2e94fefa49e5642d0141a
mgruben <GrubenM@gmail.com>
2016-09-09 12:43:28 -0500

    Handle streams of only zeros or only ones

--- KMeans.java
+++ KMeans.java
@@ -33,19 +33,38 @@ public class KMeans {
         this.clusters = clusters;
         this.rand = new Random();
         
-        stream = stream.replaceAll("^[0]+", "");
-        stream = stream.replaceAll("[0]+$", "");
-        String[] ones = stream.split("0+");
-        String[] zeros = stream.split("1+");
-        bitCollection = new String[ones.length + zeros.length - 1];
-        for (int i = 0; i < ones.length - 1; i++) {
-            bitCollection[2*i] = ones[i];
-            bitCollection[2*i+1] = zeros[i+1];
+        stream = stream.replaceAll("^[0]+", ""); // remove leading 0s
+        stream = stream.replaceAll("[0]+$", ""); // remove trailing 0s
+        
+        if (stream.length() == 0) {
+            bitCollection = new String[1];
+            bitCollection[0] = "";
         }
-        bitCollection[bitCollection.length - 1] = ones[ones.length - 1];
+        else {
+            String[] ones = stream.split("0+");
+            String[] zeros = stream.split("1+");
+
+            if (zeros.length == 0) {
+                bitCollection = new String[1];
+                bitCollection[0] = ones[0];
+            }
+            else {
+                bitCollection = new String[ones.length + zeros.length - 1];
+                for (int i = 0; i < ones.length - 1; i++) {
+                    bitCollection[2*i] = ones[i];
+                    bitCollection[2*i+1] = zeros[i+1];
+                }
+                bitCollection[bitCollection.length - 1] = ones[ones.length - 1];
+            }
+        }
+    }
+    
+    public void printBitCollection() {
+        for (String s: bitCollection) System.out.println(s);
     }
         
     public static void main(String[] args) {
-        KMeans km = new KMeans("00011011010011100000110000001111110100111110011111100000000000111011111111011111011111000000101100011111100000111110011101100000100000000", 2);
+        KMeans km = new KMeans("00000000", 2);
+        km.printBitCollection();
     }
 }
