ff78f06254fc8d3a186175c47cde5ac4f8695227
mgruben <GrubenM@gmail.com>
2016-09-09 23:16:35 -0500

    Implement time units function

--- MorseCodeDecoder.py
+++ MorseCodeDecoder.py
@@ -1,3 +1,5 @@
+import re
+
 MORSE_CODE = {
     ".-": "A",
     "-...": "B",
@@ -39,6 +41,7 @@ MORSE_CODE = {
 }
 
 heyJude = ".... . -.--   .--- ..- -.. ."
+JudeBits = "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"
 
 class Cluster(object):
     
@@ -46,12 +49,46 @@ class Cluster(object):
 
 def decodeBitsAdvanced(bits):
     '''
-    input bits, a string of 0s and 1s with variable length
+    input bits, a string of 0s and 1s with variable timing
     returns string, a morse code message
     '''
     
     # ToDo: Accept 0's and 1's, return dots, dashes and spaces
     return bits.replace('111', '-').replace('000', ' ').replace('1', '.').replace('0', '')
+
+def getTimeUnit(bits):
+    '''
+    input bits, a string of 0s and 1s with fixed timing
+    returns int, the single timing unit (i.e. dot or shortest pause)
+    '''
+    if bits == "":
+        return 0
+    o = re.split("0+", bits)
+    if len(o) == 1:
+        return len(bits)
+    if o == ['','']:
+        return 0
+    os = len(o[0])
+    for elem in o:
+        if len(elem) != os:
+            os = min(os,len(elem))
+            break
+    z = re.split("1+", bits)
+    zs = len(z[1])
+    for i in range(1, len(z) - 1):
+        if zs != len(z[i]):
+            zs = min(zs, len(z[i]))
+            break
+    return min(os, zs)
+
+
+def decodeBits(bits):
+    '''
+    input bits, a string of 0s and 1s with fixed timing
+    returns string, a morse code message
+    '''
+    bits = bits.strip("0")
+    
 
 def decodeMorse(morseCode):
     '''
