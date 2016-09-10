46116b45ed91d8c0b0a30e526be074e4b41677d9
mgruben <GrubenM@gmail.com>
2016-09-09 22:43:31 -0500

    Add API functions from problem

--- MorseCodeDecoder.py
+++ MorseCodeDecoder.py
@@ -37,3 +37,22 @@ MORSE_CODE = {
     "----.": "9",
     "...---...": "SOS"
 }
+
+class Cluster(object):
+    
+
+
+def decodeBitsAdvanced(bits):
+    '''
+    returns string, a morse code message
+    '''
+    
+    # ToDo: Accept 0's and 1's, return dots, dashes and spaces
+    return bits.replace('111', '-').replace('000', ' ').replace('1', '.').replace('0', '')
+
+def decodeMorse(morseCode):
+    '''
+    returns string, a human-readable message
+    '''
+    # ToDo: Accept dots, dashes and spaces, return human-readable message
+    return morseCode.replace('.', 'E').replace('-', 'T').replace(' ', '')
